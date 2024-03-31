c4365c6abf6dc9b2395b0c72887e03a607c601bb
Alexis Fabrigoule <alexis.fabrigoule@gmail.com>
2024-03-31 12:07:10 -0700

    Put text message on content view, remove sphere and test buttons

--- Concertina/Concertina/ContentView.swift
+++ Concertina/Concertina/ContentView.swift
@@ -20,10 +20,14 @@ struct ContentView: View {
     var body: some View {
         let synth = ConcertinaSynth()
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
+//            Model3D(named: "Scene", bundle: realityKitContentBundle)
+//                .padding(.bottom, 50)
 
-            Text("Concertina")
+            Text("Welcome to the Concertina Simulator!\n Try closing one finger at a time and then move your hands towards each other.")
+                .multilineTextAlignment(.center)
+                .font(.system(size: 30))
+                .padding()
+                .frame(width: 650)
 
             Toggle("Show concertina", isOn: $showImmersiveSpace)
                 .font(.title)
@@ -31,13 +35,13 @@ struct ContentView: View {
                 .padding(24)
                 .glassBackgroundEffect()
             
-            Button("Play test sound") {
-                synth.playTest()
-            }
-            
-            Button("Stop test sound") {
-                synth.stopTest()
-            }
+//            Button("Play test sound") {
+//                synth.playTest()
+//            }
+//            
+//            Button("Stop test sound") {
+//                synth.stopTest()
+//            }
         }
         .padding()
         .onChange(of: showImmersiveSpace) { _, newValue in
